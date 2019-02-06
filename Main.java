692b5200e89b44b2d2f1f9c3459070924dcedced
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-7.local>
2019-02-05 22:25:43 -0500

    Modified the Main class to include a method called sumToN() which prompts the user to input a number and sums from 1 through n.

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(){
 
+    }
+    public void sumToN() {
+      Scanner nInput = new Scanner(System.in);
+      System.out.print("Please enter a number");
+      int n = nInput.nextInt();
+      int sum = 0;
+      for(int i = 0; i <= n; i++) {
+          sum = sum + i;
+        }
+      System.out.print(sum);
     }
 }
